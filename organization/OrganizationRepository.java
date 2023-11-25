c3540292da374e95406df642401a48dd3a627dcd
Smriti Sharma <77639024+SmritiSharmaBG@users.noreply.github.com>
2023-11-25 17:38:01 +0530

    Create OrganizationRepository.java

---
+++ organization/OrganizationRepository.java
@@ -0,0 +1,9 @@
+package com.challenge.organization;
+
+import com.challenge.entities.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrganizationRepository extends JpaRepository<Organization, Integer> {
+}
